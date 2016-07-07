aff71a4164922eb8ce1c862e2985a25be7c20818
Mike Lee <asackofwheat@gmail.com>
2016-07-07 11:55:48 -0400

    first commit

---
+++ lib/rps2000_gem/version.rb
@@ -0,0 +1,3 @@
+module Rps2000Gem
+  VERSION = "0.1.0"
+end
---
+++ spec/rps2000_gem_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Rps2000Gem do
+  it 'has a version number' do
+    expect(Rps2000Gem::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
